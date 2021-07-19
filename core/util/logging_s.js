f2c697d939048a3ae6ee2cbfe20864a111185afa
seI3en <59121740+seI3en@users.noreply.github.com>
2021-07-19 20:29:40 +0200

    add simplified Log server version

---
+++ core/util/logging_s.js
@@ -0,0 +1,16 @@
+/*
+ * noVNC: HTML5 VNC client
+ * Copyright (C) 2019 The noVNC Authors
+ * Licensed under MPL 2.0 (see LICENSE.txt)
+ *
+ * See README.md for usage and integration instructions.
+ */
+
+/*
+ * Logging/debug routines
+ */
+
+exports.Debug = (msg) => { console.log(msg); }
+exports.Info = (msg) => { console.log(msg); };
+exports.Warn = (msg) => { console.log(msg); };
+exports.Error = (msg) => { console.log(msg); };
